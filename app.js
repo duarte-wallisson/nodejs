ab0d5bb61b964ebde4db1e7272c1d915d8deed53
duarte-wallisson <wallisson.elduarte@gmail.com>
2023-06-26 17:40:26 -0300

    feat: working with routes and files

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", function (req, res) {
-    res.send("Welcome to my app!")
+    res.sendFile(__dirname + "/html/template.html")
 })
 
 app.get("/page1", function (req, res) {
